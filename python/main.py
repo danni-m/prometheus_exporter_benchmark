0dc06017c62984304dc3d654d132b8c48ffbca3e
Danni Moiseyev <danni@redislabs.com>
2020-12-10 21:23:17 +0200

    adjust python to align to other languages

--- python/main.py
+++ python/main.py
@@ -1,4 +1,4 @@
-from prometheus_client import start_http_server, Summary, Counter, exposition
+from prometheus_client import start_http_server, Summary, Gauge, Counter, exposition
 import random
 import time
 import resource
@@ -14,18 +14,20 @@ def process_request(t):
     """A dummy function that takes some time."""
     time.sleep(t)
 
+
+
 if __name__ == '__main__':
     counters = []
-    for i in range(1000):
-        counters.append(Counter("counter{0}".format(i), "help"))
+    for i in range(2000):
+        g = Gauge("counter{0}".format(i), "help", ['number', 'foo'])
+        counters.append([i,g])
     print("After creating counters: ", resource.getrusage(0).ru_maxrss)
 
-    for i in range(10):
-        for c in counters:
-            c.inc()
+    for i, c in counters:
+       c.labels(str(i), 'bar').set(41.2)
 
     # Start up the server to expose the metrics.
-    start_http_server(8000)
+    start_http_server(8080)
     # Generate some requests.
     while True:
         process_request(random.random())
